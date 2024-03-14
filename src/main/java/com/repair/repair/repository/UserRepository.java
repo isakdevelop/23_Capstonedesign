9d3ed8c504fbf3428f0b3f05f861475ec5d9c56b
isakdevelop <daishi7462@naver.com>
2024-03-14 21:08:12 +0900

    refact - Specify a single result return type

--- src/main/java/com/repair/repair/repository/UserRepository.java
+++ src/main/java/com/repair/repair/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.repair.repair.repository;
 
 import com.repair.repair.model.User;
+import java.util.Collection;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
