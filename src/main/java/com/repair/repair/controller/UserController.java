3ab784377f66193ea356a183b99ad0d98dc2d3d0
isakdevelop <daishi7462@naver.com>
2024-03-14 09:20:00 +0900

    feat - Create UserController

---
+++ src/main/java/com/repair/repair/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.repair.repair.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+}
