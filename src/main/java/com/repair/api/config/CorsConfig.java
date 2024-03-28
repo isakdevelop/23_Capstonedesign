1b4e56a4b4edd6ac33cdfc75761109ad13e1cdb8
isakdevelop <daishi7462@naver.com>
2024-03-28 21:41:41 +0900

    feat - Create Cors and Password Config

---
+++ src/main/java/com/repair/api/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.repair.api.config;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(Collections.singletonList("*"));
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+
+}
